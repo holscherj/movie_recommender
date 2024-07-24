231e5e4661e5465328a2f7a8bd0e57fb6a6ee295
holscherj <holsche2@msu.edu>
2024-07-23 20:19:40 -0400

    Accepts user input, error handling for invalid movie title, finished

--- ml/movie_recommender.py
+++ ml/movie_recommender.py
@@ -7,7 +7,11 @@ def get_title_from_index(df, index):
 	return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(df, title):
-	return df[df.title == title]["index"].values[0]
+	try:
+		return df[df.title == title]["index"].values[0]
+	except IndexError:
+		print("I'm sorry, but we do not have that movie title in our database.")
+		print()
 
 def combine_features(row):
 	return row['genres'] + " " + row['keywords'] + " " + row['original_language'] + " " + " " + row['cast'] + " " + " " + row['director']
@@ -25,15 +29,30 @@ def main():
 	count_mat = cv.fit_transform(df['combined_features'])
 	sim_scores = cosine_similarity(count_mat)
 
-	liked_movie = "Avatar"
-	movie_index = get_index_from_title(df, liked_movie)
-	similar_movies = list(enumerate(sim_scores[movie_index]))
-	similar_movies.sort(key=lambda x: x[1], reverse=True)
+	while True:
+		prompt = "\nEnter a movie to receive similar recommendations, type 'p' to view available movie titles, or enter 'q' to quit: "
+		liked_movie = input(prompt)
 
-	print("Top Recommended Movies:")
-	print("############################################")
-	for i in range(1, 51):
-		print(get_title_from_index(df, similar_movies[i][0]))
+		if liked_movie.lower() == 'q':
+			break
+
+		if liked_movie.lower() == 'p':
+			for title in sorted(df['title']):
+				print(title)
+			continue
+
+		movie_index = get_index_from_title(df, liked_movie)
+
+		if movie_index is None:
+			continue
+
+		similar_movies = list(enumerate(sim_scores[movie_index]))
+		similar_movies.sort(key=lambda x: x[1], reverse=True)
+
+		print("\nTop Recommended Movies Similar to: " + liked_movie)
+		print("############################################")
+		for i in range(1, 51):
+			print(get_title_from_index(df, similar_movies[i][0]))
 
 
 
